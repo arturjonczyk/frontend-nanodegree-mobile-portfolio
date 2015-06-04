cd0b0439e71c6dde0ae3cda7524dec60ea4792e1
Artur Jonczyk <artur.jonczyk@gmail.com>
2015-06-04 19:05:16 +0200

    Added gruntfile.js with basic skeleton

---
+++ gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function (grunt) {
+
+    // Configure task(s)
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+    });
+
+    // Load the plugins
+    grunt.loadNpmTasks();
+
+    // Register task(s)
+    grunt.registerTask('default', []);
+};
