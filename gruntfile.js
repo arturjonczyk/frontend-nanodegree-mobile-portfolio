8340d33dd49e062e88dde24f5216be65459807db
Artur Jonczyk <artur.jonczyk@gmail.com>
2015-06-06 17:09:37 +0200

    Add BrowserSync to the grunt

--- gruntfile.js
+++ gruntfile.js
@@ -3,11 +3,28 @@ module.exports = function (grunt) {
     // Configure task(s)
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        browserSync: {
+            bsFiles: {
+                src: [
+                    'css/*.css',
+                    'index.html'
+                ]
+            },
+            options: {
+                server: {
+                    baseDir: './'
+                },
+                browser: 'google chrome',
+                notify: false,
+                //tunnel: true,
+            }
+        }
     });
 
     // Load the plugins
-    grunt.loadNpmTasks();
+    grunt.loadNpmTasks('grunt-browser-sync');
 
     // Register task(s)
     grunt.registerTask('default', []);
+    grunt.registerTask('sync', ['browserSync']);
 };
